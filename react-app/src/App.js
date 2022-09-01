89eaa565f742cda97da68db628a4e1160076d9b8
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-09-01 15:02:37 +0900

    useEffect practice

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,11 +1,41 @@
-import Button from "./Button";
 import styles from "./App.module.css"
+import {useEffect, useState} from "react";
 
 function App() {
+  const [counter, setValue] =useState(0);
+  const [keyword, setKeyword] = useState("")
+  const onClick = () => setValue((prev) => prev + 1)
+  const onChange = (event) => setKeyword(event.target.value);
+
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  },[]);
+  
+  useEffect(() => {
+    if(keyword !== "" && keyword.length > 4){
+      console.log("SEARCH FOR", keyword);
+    }
+    
+  }, [keyword]);//특정 keyword update될 때만 코드 실행
+
+  useEffect(() => {
+    console.log("I run only 'counter' changes.")
+  }, [counter]);
+
+  useEffect(() => {
+    console.log("I run only 'counter' & 'keyword' changes.")
+  }, [counter, keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back!</h1>
-      <Button text="Continue"/>
+      <input 
+        value = {keyword}
+        onChange = {onChange}
+        type="text" 
+        placeholder="Search here..."
+        />
+      <h1 className={styles.title}>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
